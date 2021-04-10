73582bcb0d603553625e20a19508864bf0a2e85e
Kawser Ahmed <kawser@doodleinc.net>
2021-04-11 01:04:07 +0600

    Node Mongoose dish with comments

--- index.js
+++ index.js
@@ -10,25 +10,41 @@ db.once('open', function() {
   console.log("Connected to Database Successfully.");
 
   Dishes.create({
-    name: 'Uthapizza',
-    description: 'Test'
+    name: 'Uthappizza',
+    description: 'test'
 })
-    .then((dish) => {
-        console.log(dish);
+.then((dish) => {
+    console.log(dish);
 
-        return Dishes.find({});
+    return Dishes.findByIdAndUpdate(dish._id, {
+        $set: { description: 'Updated test'}
+    },{ 
+        new: true 
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .exec();
+})
+.then((dish) => {
+    console.log(dish);
 
-        return Dishes.remove({});
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
+    dish.comments.push({
+        rating: 5,
+        comment: 'I\'m getting a sinking feeling!',
+        author: 'Leonardo di Carpaccio'
     });
+
+    return dish.save();
+})
+.then((dish) => {
+    console.log(dish);
+
+    return Dishes.remove({});
+})
+.then(() => {
+    return mongoose.connection.close();
+})
+.catch((err) => {
+    console.log(err);
+});
 
 });
 
